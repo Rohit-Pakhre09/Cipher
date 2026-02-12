1b35261e97e7e7fc594c8efc42fba0b2d7f8b204
Rohit <pakhrerohit281@gmail.com>
2026-02-12 22:21:47 +0530

    Fix: On refresh goes to login page.

--- backend/src/utils/utils.js
+++ backend/src/utils/utils.js
@@ -10,9 +10,9 @@ export const generateToken = (userId, res) => {
     });
 
     res.cookie("jwt", accessToken, {
-        maxAge: 7 * 24 * 60 * 60 * 1000, 
+        maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "none",
+        sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
         secure: process.env.NODE_ENV !== "development"
     });
 
