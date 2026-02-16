f43c94d539275ec06795e770e42f0d935ddf9424
Rohit <pakhrerohit281@gmail.com>
2026-02-16 06:14:48 +0530

    Fix: Login scrollbar and also deleted backup of project.

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -31,7 +31,7 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="h-[calc(100vh)] overflow-y-auto grid lg:grid-cols-2">
+    <div className="min-h-[calc(100vh-4rem)] overflow-x-hidden grid lg:grid-cols-2">
       <div className="flex flex-col justify-center items-center p-6 sm:p-12">
         <div className="w-full max-w-md space-y-6 sm:space-y-8">
           <div className="text-center">
